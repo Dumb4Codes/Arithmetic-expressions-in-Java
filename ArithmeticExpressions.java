f707f3b996f0a95d7de3475771a6c8cf47677e50
Dumb4Codes <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-11 18:40:04 +0530

    Add files via upload

---
+++ ArithmeticExpressions.java
@@ -0,0 +1,38 @@
+
+public class ArithmeticExpressions {
+
+	public static void main(String[] args) {
+		
+		// expression = operands & operators 
+		// operands = values, variables, numbers, quantity
+		// operators = + - * % 
+		
+		int  friends = 10; // for any operator
+		
+		friends = friends + 2;
+		
+		//there's a shortcut for adding and subtracting and that is "++" and "--" by using friends++;
+		
+				
+		System.out.println(friends);
+		
+		//If want decimals in result
+		
+		double food = 10;
+				food = (double)food / 3;
+				System.out.println(food);
+				
+				
+		 double a = 4;
+		 double b = 6%3; //Modulo Operator = 6 divided by 5 and result the remainder
+				
+				
+		 System.out.println(b);
+		
+		 
+ 
+
+
+	}
+
+}
